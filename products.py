69df715feed192369ebdd213286e0fba93711aac
Andy Huang <andyex88@gmail.com>
2022-11-21 14:05:38 -0700

    write in file

--- products.py
+++ products.py
@@ -8,4 +8,8 @@ while True:
 print(products)
 
 for p in products:
-	print(p)
+	print(p[0], 'price is', p[1])
+
+with open('products.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
